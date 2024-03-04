dda2ba040651ae142484d0e376a0a7a6173b2e74
Johan <Johandyrlund@hotmail.com>
2024-03-04 11:44:46 +0100

    g

--- Project-MethCooker/Assets/Johan/Scripts/Cooker.cs
+++ Project-MethCooker/Assets/Johan/Scripts/Cooker.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cooker : MonoBehaviour
 {
+    public Ingredients[] Ingredientsneeded = new Ingredients[10];
+    public NoteCreater CurrentNote;
+
+    private Ingredients addIngrediant;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,16 @@ public class Cooker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateIngrediants();
+    }
+
+    public void UpdateIngrediants()
+    {
         
+        for (int i = 0; i < CurrentNote.NamesOfIngrdients.Length; i++)
+        {
+            Ingredientsneeded[i] = CurrentNote.NamesOfIngrdients[i];
+            addIngrediant = Ingredientsneeded[i];
+        }
     }
 }
