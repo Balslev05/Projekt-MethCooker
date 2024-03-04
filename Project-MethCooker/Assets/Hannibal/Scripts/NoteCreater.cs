9bdd2916bd5d29ff5100c7f786178064666e1e50
HannibalBlaabjerg <hannibal@blab.dk>
2024-03-04 11:13:29 +0100

    noteCreater
    NOTES YEA BABY

---
+++ Project-MethCooker/Assets/Hannibal/Scripts/NoteCreater.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Note", menuName = "NoteCreater", order = 0)]
+public class NoteCreater : ScriptableObject
+{
+    public string NameOfDrug;
+    public string[] NamesOfIngrdients;
+
+}
