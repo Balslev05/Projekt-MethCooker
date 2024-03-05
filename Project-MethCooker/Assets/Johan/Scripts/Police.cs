87b621f90484c7ebafd149740396dd82d571e99e
Johan <Johandyrlund@hotmail.com>
2024-03-05 11:33:49 +0100

    s

---
+++ Project-MethCooker/Assets/Johan/Scripts/Police.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police : MonoBehaviour
+{
+    // Start is called before the first frame update
+    PlayerMovement player;
+    public float wantedMeter;
+    void Start()
+    {
+        //player.jumping
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
