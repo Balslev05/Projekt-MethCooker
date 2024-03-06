250b06f7d4ef26b78d9f70a4eeefd21f932933b1
Johan <Johandyrlund@hotmail.com>
2024-03-06 09:14:51 +0100

    students

--- Project-MethCooker/Assets/Police.cs
+++ Project-MethCooker/Assets/Police.cs
@@ -10,7 +10,7 @@ public class Police : MonoBehaviour
     public string levelname;
     public PlayerMovement Morten;
     public float policetimer;
-    
+
     // Start is called before the first frame update
     void Start()
     {
